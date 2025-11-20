a582c87b8903e52f1f782c9c2a190bec9467085e
Nadir Koudri <nadir.koudri@epita.fr>
2025-11-20 16:42:17 +0100

    Implementation des virements et du decouvert done

--- src/main/java/com/example/Projet_Nadir_Koudri/service/TransferService.java
+++ src/main/java/com/example/Projet_Nadir_Koudri/service/TransferService.java
@@ -1,21 +1,40 @@
 package com.example.Projet_Nadir_Koudri.service;
 
 import com.example.Projet_Nadir_Koudri.domain.account.Account;
+import com.example.Projet_Nadir_Koudri.infrastructure.repository.AccountRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransferService {
 
-    public boolean transfer(Account from, Account to, double amount) {
+    private final AccountRepository repo;
+
+    public TransferService(AccountRepository repo) {
+        this.repo = repo;
+    }
+
+    public boolean transfer(Long fromId, Long toId, double amount) {
+
         if (amount <= 0) return false;
 
+        Account from = repo.findById(fromId).orElse(null);
+        Account to = repo.findById(toId).orElse(null);
+
+        if (from == null || to == null)
+            return false;
+
         double oldBalance = from.getBalance();
+
         from.withdraw(amount);
 
-        if (from.getBalance() != oldBalance) {
-            to.deposit(amount);
-            return true;
-        }
-        return false;
+        if (from.getBalance() == oldBalance)
+            return false;
+
+        to.deposit(amount);
+
+        repo.save(from);
+        repo.save(to);
+
+        return true;
     }
 }
--- src/main/java/com/example/Projet_Nadir_Koudri/web/controller/TransferController.java
+++ src/main/java/com/example/Projet_Nadir_Koudri/web/controller/TransferController.java
@@ -1,7 +1,5 @@
 package com.example.Projet_Nadir_Koudri.web.controller;
 
-import com.example.Projet_Nadir_Koudri.domain.account.Account;
-import com.example.Projet_Nadir_Koudri.infrastructure.repository.AccountRepository;
 import com.example.Projet_Nadir_Koudri.service.TransferService;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,23 +8,15 @@ import org.springframework.web.bind.annotation.*;
 public class TransferController {
 
     private final TransferService service;
-    private final AccountRepository repo;
 
-    public TransferController(TransferService service, AccountRepository repo) {
+    public TransferController(TransferService service) {
         this.service = service;
-        this.repo = repo;
     }
 
     @PostMapping
     public boolean transfer(@RequestParam Long fromId,
                             @RequestParam Long toId,
                             @RequestParam double amount) {
-
-        Account from = repo.findById(fromId).orElse(null);
-        Account to = repo.findById(toId).orElse(null);
-
-        if (from == null || to == null) return false;
-
-        return service.transfer(from, to, amount);
+        return service.transfer(fromId, toId, amount);
     }
 }
