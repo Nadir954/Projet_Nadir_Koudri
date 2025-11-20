3a00802cc1fb4fb2a00da6625f5cd8cdf75d348c
Nadir Koudri <nadir.koudri@epita.fr>
2025-11-20 14:19:53 +0100

    Initial commit - Project_Nadir_Koudri

---
+++ src/main/java/com/example/Projet_Nadir_Koudri/ProjetNadirKoudriApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projet_Nadir_Koudri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetNadirKoudriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetNadirKoudriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Projet_Nadir_Koudri/ProjetNadirKoudriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projet_Nadir_Koudri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetNadirKoudriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
